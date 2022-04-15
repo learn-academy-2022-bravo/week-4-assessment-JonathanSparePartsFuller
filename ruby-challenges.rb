b7edd420d9ec6bff403e3785e87c817b60dfde79
JonathnSparePartsFuller <jonefuller@gmail.com>
2022-04-15 14:19:10 -0700

    Create a method that takes in a number and determines if the number is even or odd. Use the test variables provided.

--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -2,6 +2,10 @@
 # MINASWAN
 
 # --------------------1) Create a method that takes in a number and determines if the number is even or odd. Use the test variables provided.
+# Psuedo Code
+# I need to make conditional statements for this method, which I'll call even_or_odd
+# I'll use string interperlation, .even, and .odd
+# I'll also create a new variable to take in all three variables so I can call each one individually
 
 num1 = 7
 # Expected output: '7 is odd'
@@ -10,7 +14,22 @@ num2 = 42
 num3 = 221
 # Expected output: '221 is odd'
 
+stored_nums = num1, num2, num3
 
+def even_or_odd array
+    array.select do |value|
+        if value.odd?
+        puts "#{value.to_s} is odd."
+        else
+        puts "#{value.to_s} is even."
+        end
+    end
+end
+p even_or_odd stored_nums
+
+# Output:7 is odd.
+        # 42 is even.
+        # 221 is odd.
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
 
@@ -21,7 +40,12 @@ album2 = 'Sgt Pepper'
 album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
-
+# def delete
+#     "#{album1}.delete"
+# end
+# p delete
+# album1.delete! 'a', 'e', 'i', 'o', 'u'
+#     p album1
 
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forward or backward. Use the test variables provided.
 
